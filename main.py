bfe0d9c041d0b23483519dd0064141651e91a20a
itsadn <its.adngaming@gmail.com>
2022-03-30 00:40:51 -0400

    [Clean:]Comment all main file

--- main.py
+++ main.py
@@ -18,15 +18,6 @@ from modules.pxrfAnCalculator import PxrfAnCalculator
 # UNKNOWN_DATA = pxrf.format_data(UNKNOWN_DATA)
 # UNKNOWN_DATA_ELEMENTS = extract_elements(UNKNOWN_DATA)
 
-# REFERENCE PROBE DATA
-# workbook, file = mf.open_xlsx_file_('data/probe_data.xlsx')
-# HEAD_PROBE_REFERENCE_DATA_VALUE, PROBE_REFERENCE_DATA = mf.compile_xlsx_to_dict(workbook, file)
-# print(HEAD_PROBE_REFERENCE_DATA_VALUE)
-# print(PROBE_REFERENCE_DATA)
-# calibration_chemical_element, non_convertible_oxid = cb.convert_oxid_to_element(PROBE_REFERENCE_DATA)
-# print('---------------------------------')
-# print('Chemical: ', calibration_chemical_element)
-# print('Non-convertible: ', non_convertible_oxid)
 
 # CALIBRATION PXRF DATA
 # workbook, file = mf.open_xlsx_file_('data/calibration_pxrf.xlsx')
