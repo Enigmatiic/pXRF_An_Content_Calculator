8b48a2ca79530db90b44e5ce3d47f09645416565
itsadn <its.adngaming@gmail.com>
2022-03-30 00:40:20 -0400

    [UI]: Remain

--- ui/ui_interface.py
+++ ui/ui_interface.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'interfacesALVsL.ui'
+## Form generated from reading UI file 'interfacewaicwl.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.2.4
 ##
@@ -9,16 +9,17 @@
 ################################################################################
 
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
+                            QMetaObject, QObject, QPoint, QRect,
+                            QSize, QTime, QUrl, Qt)
 from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
+                           QFont, QFontDatabase, QGradient, QIcon,
+                           QImage, QKeySequence, QLinearGradient, QPainter,
+                           QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QFrame,
-    QGraphicsView, QGridLayout, QGroupBox, QHeaderView,
-    QLabel, QPushButton, QSizePolicy, QTabWidget,
-    QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget)
+                               QGraphicsView, QGridLayout, QGroupBox, QHeaderView,
+                               QLabel, QPushButton, QSizePolicy, QTabWidget,
+                               QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget)
+
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -35,16 +36,15 @@ class Ui_Form(object):
         self.frame_3.setFrameShadow(QFrame.Raised)
         self.gridLayout_4 = QGridLayout(self.frame_3)
         self.gridLayout_4.setObjectName(u"gridLayout_4")
-        self.label = QLabel(self.frame_3)
-        self.label.setObjectName(u"label")
-        self.label.setLayoutDirection(Qt.LeftToRight)
-        self.label.setPixmap(QPixmap(u"ui/img/logo_1.png"))
-        self.label.setScaledContents(False)
-        self.label.setAlignment(Qt.AlignCenter)
-        self.label.setWordWrap(True)
+        self.l_img_logo = QLabel(self.frame_3)
+        self.l_img_logo.setObjectName(u"l_img_logo")
+        self.l_img_logo.setLayoutDirection(Qt.LeftToRight)
+        self.l_img_logo.setPixmap(QPixmap(u"ui/img/logo_1.png"))
+        self.l_img_logo.setScaledContents(False)
+        self.l_img_logo.setAlignment(Qt.AlignCenter)
+        self.l_img_logo.setWordWrap(True)
 
-        self.gridLayout_4.addWidget(self.label, 0, 0, 1, 1)
-
+        self.gridLayout_4.addWidget(self.l_img_logo, 0, 0, 1, 1)
 
         self.gridLayout_11.addWidget(self.frame_3, 0, 0, 2, 1)
 
@@ -64,12 +64,11 @@ class Ui_Form(object):
         font.setItalic(False)
         self.label_app_title.setFont(font)
         self.label_app_title.setStyleSheet(u"font: 700 24pt \"Poppins\";\n"
-"color: rgb(42, 42, 42);")
+                                           "color: rgb(42, 42, 42);")
         self.label_app_title.setScaledContents(True)
         self.label_app_title.setAlignment(Qt.AlignCenter)
 
         self.gridLayout_10.addWidget(self.label_app_title, 0, 0, 1, 1)
-
 
         self.gridLayout_11.addWidget(self.frame_app_title, 0, 2, 1, 1)
 
@@ -80,14 +79,14 @@ class Ui_Form(object):
 
         self.gridLayout_11.addWidget(self.verticalLine_divider, 1, 1, 7, 1)
 
-        self.frame = QFrame(Form)
-        self.frame.setObjectName(u"frame")
-        self.frame.setStyleSheet(u"background-color: rgba(217, 217, 217, .4);")
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Raised)
-        self.gridLayout = QGridLayout(self.frame)
+        self.frame_charts = QFrame(Form)
+        self.frame_charts.setObjectName(u"frame_charts")
+        self.frame_charts.setStyleSheet(u"background-color: rgba(217, 217, 217, .4);")
+        self.frame_charts.setFrameShape(QFrame.StyledPanel)
+        self.frame_charts.setFrameShadow(QFrame.Raised)
+        self.gridLayout = QGridLayout(self.frame_charts)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.gv_chart = QGraphicsView(self.frame)
+        self.gv_chart = QGraphicsView(self.frame_charts)
         self.gv_chart.setObjectName(u"gv_chart")
         self.gv_chart.viewport().setProperty("cursor", QCursor(Qt.CrossCursor))
         self.gv_chart.setStyleSheet(u"background-color: rgb(255, 255, 255);")
@@ -95,8 +94,7 @@ class Ui_Form(object):
 
         self.gridLayout.addWidget(self.gv_chart, 0, 0, 1, 1)
 
-
-        self.gridLayout_11.addWidget(self.frame, 1, 2, 4, 1)
+        self.gridLayout_11.addWidget(self.frame_charts, 1, 2, 4, 1)
 
         self.gb_importFile = QGroupBox(Form)
         self.gb_importFile.setObjectName(u"gb_importFile")
@@ -113,27 +111,26 @@ class Ui_Form(object):
 
         self.verticalLayout_6.addWidget(self.l_typeFile)
 
-        self.cb_chooseTypeFile = QComboBox(self.gb_importFile)
-        self.cb_chooseTypeFile.addItem("")
+        self.select_chooseTypeFile = QComboBox(self.gb_importFile)
+        self.select_chooseTypeFile.addItem("")
         icon = QIcon()
         icon.addFile(u"ui/img/xlsx_.ico", QSize(), QIcon.Normal, QIcon.Off)
-        self.cb_chooseTypeFile.addItem(icon, "")
-        self.cb_chooseTypeFile.addItem(icon, "")
-        self.cb_chooseTypeFile.addItem(icon, "")
-        self.cb_chooseTypeFile.setObjectName(u"cb_chooseTypeFile")
-        self.cb_chooseTypeFile.setCursor(QCursor(Qt.ArrowCursor))
+        self.select_chooseTypeFile.addItem(icon, "")
+        self.select_chooseTypeFile.addItem(icon, "")
+        self.select_chooseTypeFile.addItem(icon, "")
+        self.select_chooseTypeFile.setObjectName(u"select_chooseTypeFile")
+        self.select_chooseTypeFile.setCursor(QCursor(Qt.ArrowCursor))
 
-        self.verticalLayout_6.addWidget(self.cb_chooseTypeFile)
+        self.verticalLayout_6.addWidget(self.select_chooseTypeFile)
 
-        self.tb_browserFile = QPushButton(self.gb_importFile)
-        self.tb_browserFile.setObjectName(u"tb_browserFile")
-        self.tb_browserFile.setCursor(QCursor(Qt.PointingHandCursor))
+        self.pb_browserFile = QPushButton(self.gb_importFile)
+        self.pb_browserFile.setObjectName(u"pb_browserFile")
+        self.pb_browserFile.setCursor(QCursor(Qt.PointingHandCursor))
         icon1 = QIcon()
         icon1.addFile(u"ui/img/import.ico", QSize(), QIcon.Normal, QIcon.Off)
-        self.tb_browserFile.setIcon(icon1)
+        self.pb_browserFile.setIcon(icon1)
 
-        self.verticalLayout_6.addWidget(self.tb_browserFile)
-
+        self.verticalLayout_6.addWidget(self.pb_browserFile)
 
         self.gridLayout_11.addWidget(self.gb_importFile, 2, 0, 1, 1)
 
@@ -158,13 +155,12 @@ class Ui_Form(object):
 
         self.verticalLayout_23.addWidget(self.cb_pxrfCalibration)
 
-        self.cb_unknowAnalysis = QCheckBox(self.gb_validation)
-        self.cb_unknowAnalysis.setObjectName(u"cb_unknowAnalysis")
-        self.cb_unknowAnalysis.setIcon(icon)
-        self.cb_unknowAnalysis.setCheckable(False)
+        self.cb_unknownAnalysis = QCheckBox(self.gb_validation)
+        self.cb_unknownAnalysis.setObjectName(u"cb_unknownAnalysis")
+        self.cb_unknownAnalysis.setIcon(icon)
+        self.cb_unknownAnalysis.setCheckable(False)
 
-        self.verticalLayout_23.addWidget(self.cb_unknowAnalysis)
-
+        self.verticalLayout_23.addWidget(self.cb_unknownAnalysis)
 
         self.gridLayout_11.addWidget(self.gb_validation, 3, 0, 1, 1)
 
@@ -190,7 +186,6 @@ class Ui_Form(object):
         self.pb_formatPXRF.setIcon(icon2)
 
         self.verticalLayout_9.addWidget(self.pb_formatPXRF)
-
 
         self.gridLayout_11.addWidget(self.gb_calibration, 4, 0, 1, 1)
 
@@ -221,7 +216,7 @@ class Ui_Form(object):
         self.pb_start.setFont(font2)
         self.pb_start.setCursor(QCursor(Qt.PointingHandCursor))
         self.pb_start.setStyleSheet(u"color: rgb(44, 202, 57);\n"
-"background-color: rgb(85, 255, 127);")
+                                    "background-color: rgb(85, 255, 127);")
         icon4 = QIcon()
         icon4.addFile(u"ui/img/start.ico", QSize(), QIcon.Normal, QIcon.Off)
         self.pb_start.setIcon(icon4)
@@ -229,17 +224,16 @@ class Ui_Form(object):
 
         self.verticalLayout_8.addWidget(self.pb_start)
 
-
         self.gridLayout_11.addWidget(self.gb_traitment, 5, 0, 1, 1)
 
-        self.frame_2 = QFrame(Form)
-        self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setStyleSheet(u"background-color: rgba(217, 217, 217, .4);")
-        self.frame_2.setFrameShape(QFrame.StyledPanel)
-        self.frame_2.setFrameShadow(QFrame.Raised)
-        self.gridLayout_3 = QGridLayout(self.frame_2)
+        self.frame_tabs = QFrame(Form)
+        self.frame_tabs.setObjectName(u"frame_tabs")
+        self.frame_tabs.setStyleSheet(u"background-color: rgba(217, 217, 217, .4);")
+        self.frame_tabs.setFrameShape(QFrame.StyledPanel)
+        self.frame_tabs.setFrameShadow(QFrame.Raised)
+        self.gridLayout_3 = QGridLayout(self.frame_tabs)
         self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.tw_tabs = QTabWidget(self.frame_2)
+        self.tw_tabs = QTabWidget(self.frame_tabs)
         self.tw_tabs.setObjectName(u"tw_tabs")
         font3 = QFont()
         font3.setFamilies([u"Poppins"])
@@ -250,57 +244,56 @@ class Ui_Form(object):
         self.tab_calibration_probe.setObjectName(u"tab_calibration_probe")
         self.gridLayout_6 = QGridLayout(self.tab_calibration_probe)
         self.gridLayout_6.setObjectName(u"gridLayout_6")
-        self.tableWidget = QTableWidget(self.tab_calibration_probe)
-        self.tableWidget.setObjectName(u"tableWidget")
+        self.table_calibration_probe = QTableWidget(self.tab_calibration_probe)
+        self.table_calibration_probe.setObjectName(u"table_calibration_probe")
 
-        self.gridLayout_6.addWidget(self.tableWidget, 0, 0, 1, 1)
+        self.gridLayout_6.addWidget(self.table_calibration_probe, 0, 0, 1, 1)
 
         self.tw_tabs.addTab(self.tab_calibration_probe, "")
         self.tab_calibration_pxrf = QWidget()
         self.tab_calibration_pxrf.setObjectName(u"tab_calibration_pxrf")
         self.gridLayout_5 = QGridLayout(self.tab_calibration_pxrf)
         self.gridLayout_5.setObjectName(u"gridLayout_5")
-        self.tableWidget_2 = QTableWidget(self.tab_calibration_pxrf)
-        self.tableWidget_2.setObjectName(u"tableWidget_2")
+        self.table_calibration_pxrf = QTableWidget(self.tab_calibration_pxrf)
+        self.table_calibration_pxrf.setObjectName(u"table_calibration_pxrf")
 
-        self.gridLayout_5.addWidget(self.tableWidget_2, 0, 0, 1, 1)
+        self.gridLayout_5.addWidget(self.table_calibration_pxrf, 0, 0, 1, 1)
 
         self.tw_tabs.addTab(self.tab_calibration_pxrf, "")
         self.tab_correction_factor = QWidget()
         self.tab_correction_factor.setObjectName(u"tab_correction_factor")
         self.gridLayout_7 = QGridLayout(self.tab_correction_factor)
         self.gridLayout_7.setObjectName(u"gridLayout_7")
-        self.tableWidget_3 = QTableWidget(self.tab_correction_factor)
-        self.tableWidget_3.setObjectName(u"tableWidget_3")
+        self.table_correction_factor = QTableWidget(self.tab_correction_factor)
+        self.table_correction_factor.setObjectName(u"table_correction_factor")
 
-        self.gridLayout_7.addWidget(self.tableWidget_3, 0, 0, 1, 1)
+        self.gridLayout_7.addWidget(self.table_correction_factor, 0, 0, 1, 1)
 
         self.tw_tabs.addTab(self.tab_correction_factor, "")
         self.tab_pxrf_analysis = QWidget()
         self.tab_pxrf_analysis.setObjectName(u"tab_pxrf_analysis")
         self.gridLayout_8 = QGridLayout(self.tab_pxrf_analysis)
         self.gridLayout_8.setObjectName(u"gridLayout_8")
-        self.tableWidget_4 = QTableWidget(self.tab_pxrf_analysis)
-        self.tableWidget_4.setObjectName(u"tableWidget_4")
+        self.table_pxrf_analysis = QTableWidget(self.tab_pxrf_analysis)
+        self.table_pxrf_analysis.setObjectName(u"table_pxrf_analysis")
 
-        self.gridLayout_8.addWidget(self.tableWidget_4, 0, 0, 1, 1)
+        self.gridLayout_8.addWidget(self.table_pxrf_analysis, 0, 0, 1, 1)
 
         self.tw_tabs.addTab(self.tab_pxrf_analysis, "")
         self.tab_results = QWidget()
         self.tab_results.setObjectName(u"tab_results")
         self.gridLayout_9 = QGridLayout(self.tab_results)
         self.gridLayout_9.setObjectName(u"gridLayout_9")
-        self.tableWidget_5 = QTableWidget(self.tab_results)
-        self.tableWidget_5.setObjectName(u"tableWidget_5")
+        self.table_results = QTableWidget(self.tab_results)
+        self.table_results.setObjectName(u"table_results")
 
-        self.gridLayout_9.addWidget(self.tableWidget_5, 0, 0, 1, 1)
+        self.gridLayout_9.addWidget(self.table_results, 0, 0, 1, 1)
 
         self.tw_tabs.addTab(self.tab_results, "")
 
         self.gridLayout_3.addWidget(self.tw_tabs, 0, 0, 1, 1)
 
-
-        self.gridLayout_11.addWidget(self.frame_2, 5, 2, 3, 1)
+        self.gridLayout_11.addWidget(self.frame_tabs, 5, 2, 3, 1)
 
         self.gb_project = QGroupBox(Form)
         self.gb_project.setObjectName(u"gb_project")
@@ -326,7 +319,6 @@ class Ui_Form(object):
 
         self.gridLayout_2.addWidget(self.pb_newProject, 0, 0, 1, 1)
 
-
         self.gridLayout_11.addWidget(self.gb_project, 6, 0, 1, 1)
 
         self.gb_resultCalcicite = QGroupBox(Form)
@@ -345,35 +337,33 @@ class Ui_Form(object):
 
         self.verticalLayout_15.addWidget(self.pb_extractResult)
 
-
         self.gridLayout_11.addWidget(self.gb_resultCalcicite, 7, 0, 1, 1)
-
 
         self.retranslateUi(Form)
 
         self.pb_start.setDefault(True)
-        self.tw_tabs.setCurrentIndex(0)
-
+        self.tw_tabs.setCurrentIndex(4)
 
         QMetaObject.connectSlotsByName(Form)
+
     # setupUi
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"pXRF An Calculator", None))
-        self.label.setText("")
+        self.l_img_logo.setText("")
         self.label_app_title.setText(QCoreApplication.translate("Form", u"CALCICIT\u00c9 PLAGIOCLASE", None))
         self.gb_importFile.setTitle(QCoreApplication.translate("Form", u"Importer un fichier", None))
         self.l_typeFile.setText(QCoreApplication.translate("Form", u"Type de donn\u00e9es", None))
-        self.cb_chooseTypeFile.setItemText(0, QCoreApplication.translate("Form", u"- Choisir un type de donn\u00e9e -", None))
-        self.cb_chooseTypeFile.setItemText(1, QCoreApplication.translate("Form", u"Microsonde - Donn\u00e9es de ref\u00e9rence", None))
-        self.cb_chooseTypeFile.setItemText(2, QCoreApplication.translate("Form", u"Analyses pXRF - Donn\u00e9es de calibration", None))
-        self.cb_chooseTypeFile.setItemText(3, QCoreApplication.translate("Form", u"Analyses pXRF - Donn\u00e9es inconnues", None))
+        self.select_chooseTypeFile.setItemText(0, QCoreApplication.translate("Form", u"- Choisir un type de donn\u00e9e -", None))
+        self.select_chooseTypeFile.setItemText(1, QCoreApplication.translate("Form", u"Microsonde - Donn\u00e9es de ref\u00e9rence", None))
+        self.select_chooseTypeFile.setItemText(2, QCoreApplication.translate("Form", u"Analyses pXRF - Donn\u00e9es de calibration", None))
+        self.select_chooseTypeFile.setItemText(3, QCoreApplication.translate("Form", u"Analyses pXRF - Donn\u00e9es inconnues", None))
 
-        self.tb_browserFile.setText(QCoreApplication.translate("Form", u"Parcourir ...", None))
+        self.pb_browserFile.setText(QCoreApplication.translate("Form", u"Parcourir ...", None))
         self.gb_validation.setTitle(QCoreApplication.translate("Form", u"Validation", None))
         self.cb_probeReference.setText(QCoreApplication.translate("Form", u"Donn\u00e9es de Ref\u00e9rence - Microsonde", None))
         self.cb_pxrfCalibration.setText(QCoreApplication.translate("Form", u"Donn\u00e9es de Calibration - pXRF", None))
-        self.cb_unknowAnalysis.setText(QCoreApplication.translate("Form", u"Donn\u00e9es d'analyses inconnu - pXRF", None))
+        self.cb_unknownAnalysis.setText(QCoreApplication.translate("Form", u"Donn\u00e9es d'analyses inconnu - pXRF", None))
         self.gb_calibration.setTitle(QCoreApplication.translate("Form", u"Calibration", None))
         self.pb_formatProbe.setText(QCoreApplication.translate("Form", u"Format -microsonde", None))
         self.pb_formatPXRF.setText(QCoreApplication.translate("Form", u"Format - pXRF", None))
@@ -392,4 +382,3 @@ class Ui_Form(object):
         self.gb_resultCalcicite.setTitle(QCoreApplication.translate("Form", u"R\u00e9sultat calcicit\u00e9", None))
         self.pb_extractResult.setText(QCoreApplication.translate("Form", u"Extraire - Excel", None))
     # retranslateUi
-
